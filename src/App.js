6b52f57b839638a9fdd39e5f1d4e5b360680b931
Liat Meadows <liatmeadows@gmail.com>
2023-09-08 20:11:54 +0300

    meadows - logo spin animation, style, delete lannguage provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n/i18n';
-import PropTypes from 'prop-types';
-
-import { LanguageProvider } from './components/LanguageContext';
 
 import HomeScreen from './components/homeScreen';
 import Header from './ui/header';
@@ -35,35 +32,5 @@ function App() {
     </>
   );
 }
-// function App({ setIsHebrew }) {
-//   return (
-//     <>
-//       <I18nextProvider i18n={i18n}>
-//         <LanguageProvider setIsHebrew={setIsHebrew}>
-//           <Router>
-//             <Header setIsHebrew={setIsHebrew} />
-//             <div className="main">
-//               <Switch>
-//                 <Route path="/" exact component={HomeScreen} />
-//                 <Route path="/development" component={Development} />
-//                 <Route path="/design" component={Design} />
-//                 <Route path="/about" component={About} />
-//                 <Route
-//                   path="/projectDetails/:slug"
-//                   component={ProjectDetails}
-//                 />
-//               </Switch>
-//             </div>
-//             <Footer />
-//           </Router>
-//         </LanguageProvider>
-//       </I18nextProvider>
-//     </>
-//   );
-// }
-
-// App.propTypes = {
-//   setIsHebrew: PropTypes.func.isRequired,
-// };
 
 export default App;
--- src/components/logo.js
+++ src/components/logo.js
@@ -1,5 +1,11 @@
 import logo from '../assets/icons/logo.svg';
 
 export default function Logo() {
-  return <img className="app-logo" src={logo} alt="liat-meadows-logo" />;
+  return (
+    <img
+      className="app-logo rotate-center"
+      src={logo}
+      alt="liat-meadows-logo"
+    />
+  );
 }

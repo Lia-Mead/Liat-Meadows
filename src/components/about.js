5eca575ac64ca9dc29479599af3c29555aa9cd00
Liat Meadows <liatmeadows@gmail.com>
2024-02-03 15:44:22 +0100

    meadows - color scheme changes, capsules in about

--- src/components/about.js
+++ src/components/about.js
@@ -37,13 +37,31 @@ const About = () => {
             <br />
             <br />
             <img className="icon" src={StackIcon} alt="liat-meadows" />
-            <p>
+            <div className='pill-box'>
+              <button className="pill">JavaScript</button>
+              <button className="pill">HTML5</button>
+              <button className="pill">CSS</button>
+              <button className="pill">SASS</button>
+              <button className="pill">Node.js</button>
+              <button className="pill">Redux</button>
+              <button className="pill">Bootstrap</button>
+              <button className="pill">Git</button>
+              <button className="pill">GitHub</button>
+              <button className="pill">jQuery</button>
+              <button className="pill">Wordpress</button>
+              <button className="pill">Socket.IO</button>
+              <button className="pill">Socket.IO</button>
+              <button className="pill">Sketch</button>
+              <button className="pill">Adobe Creative Suite</button>
+            </div>
+   
+            {/* <p>
               JavaScript &#8226; HTML5 &#8226; CSS &#8226; SASS &#8226; Node.js
               &#8226; Express &#8226; React-Native &#8226; React.js &#8226;
               Redux &#8226; Bootstrap &#8226; Git &#8226; GitHub &#8226; jQuery
               &#8226; Wordpress &#8226; Socket.IO &#8226; Sketch &#8226; Adobe
               Creative Suite
-            </p>
+            </p> */}
           </div>
         </div>
 
